1db42f61899e7ce27a067f5f15093e2034bdee70
nacosking <teohyihern28@gmail.com>
2025-12-24 03:24:30 +0800

    update database

--- Cloud/database.tf
+++ Cloud/database.tf
@@ -4,7 +4,7 @@
 resource "aws_db_instance" "main" {
   identifier               = "${var.project_name}-db"
   engine                   = "postgres"
-  engine_version           = "15.4"
+  engine_version           = "16.3"
   instance_class           = "db.t3.micro"
 
   # --- Role 3: Security ---
