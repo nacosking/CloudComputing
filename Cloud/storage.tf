204f7ba342f17206b0a10b1f8c14217e898e6896
tinggyi <tingyibotak@gmail.com>
2025-12-24 22:44:43 +0800

    fix

--- Cloud/storage.tf
+++ Cloud/storage.tf
@@ -31,9 +31,25 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "app_storage" {
 resource "aws_s3_bucket_public_access_block" "app_storage" {
   bucket = aws_s3_bucket.app_storage.id
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.app_storage.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.app_storage.arn}/*"
+      },
+    ]
+  })
 }
 
 # Lifecycle policy (Cost Optimization)
