d3b8ff78d23176c4769254a72e1a33510f167307
tinggyi <tingyibotak@gmail.com>
2025-12-25 17:33:25 +0800

    public access to aws s3 bucket

--- Cloud/storage.tf
+++ Cloud/storage.tf
@@ -30,9 +30,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "app_storage" {
 # Block public access (Security Hardening)
 resource "aws_s3_bucket_public_access_block" "app_storage" {
   bucket = aws_s3_bucket.app_storage.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
