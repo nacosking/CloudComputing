91d997ad21d60d1120d31f5881ec93430d97b160
tinggyi <tingyibotak@gmail.com>
2025-12-26 21:17:25 +0800

    fixed auth-context

--- Reserve-Menu/client/src/contexts/auth-context.tsx
+++ Reserve-Menu/client/src/contexts/auth-context.tsx
@@ -101,6 +101,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
+      credentials: "include",
     });
 
     if (!res.ok) {
